40d4729f4fab421755b02480b11ee3c022459979
Vinh Nguyen <vinhnguyen@Vinhs-MacBook-Air.local>
2025-09-18 13:19:24 +0700

    fixed test123

--- app/components/Invoice/Invoice.js
+++ app/components/Invoice/Invoice.js
@@ -123,41 +123,30 @@ const InvoiceContentInner = forwardRef((props, ref) => {
           <strong>{invoiceInfo.address}</strong>
         </p>
       </div>
-
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          overflow: "hidden",
-          width: "100%",
-          marginTop: "0.5rem",
-        }}
-      >
-        <table className="table-invoice">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Tên</th>
-              <th>SL</th>
-              <th>ĐG</th>
-              <th>TT</th>
-            </tr>
-          </thead>
-          <tbody>
-            {invoiceInfo.items.map((item, i) => {
-              return (
-                <tr key={i + "tbinvocie"}>
-                  <td>{i + 1}</td>
-                  <td>{item?.name}</td>
-                  <td>{item?.qty}</td>
-                  <td>{formatNumber(item?.price || 0)}</td>
-                  <td>{formatNumber((item?.qty || 0) * (item?.price || 0))}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+      <table className="table-invoice">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Tên</th>
+            <th>SL</th>
+            <th>ĐG</th>
+            <th>TT</th>
+          </tr>
+        </thead>
+        <tbody>
+          {invoiceInfo.items.map((item, i) => {
+            return (
+              <tr key={i + "tbinvocie"}>
+                <td>{i + 1}</td>
+                <td>{item?.name}</td>
+                <td>{item?.qty}</td>
+                <td>{formatNumber(item?.price || 0)}</td>
+                <td>{formatNumber((item?.qty || 0) * (item?.price || 0))}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
       <Divider className="mt-1 mb-1" />
       <div className="d-flex justify-content-between mt-2">
         <p>
